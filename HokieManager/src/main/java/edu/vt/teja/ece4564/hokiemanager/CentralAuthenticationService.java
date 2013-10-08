9b59d809c7f82591b6de5cf4c74ad95728c8ed0b
Teja Chiluvuri <tejachil@vt.edu>
2013-10-08 19:08:38 -0400

    Created login layout and CentralAuthenticationService class.

---
+++ HokieManager/src/main/java/edu/vt/teja/ece4564/hokiemanager/CentralAuthenticationService.java
@@ -0,0 +1,11 @@
+package edu.vt.teja.ece4564.hokiemanager;
+
+/**
+ * Created by teja on 10/8/13.
+ */
+public class CentralAuthenticationService {
+    private final static String HOST = "auth.vt.edu";
+    private final static String ORIGIN = "https://auth.vt.edu";
+    private final static String LOGIN_URL = "https://auth.vt.edu/login";
+    private final static String LOGOUT_URL = "https://auth.vt.edu/logout";
+}
